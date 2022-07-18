5861c3b56d375bd9a4c8211a79d1d4a80d01dbae
levan-dolidze <64006772+levan-dolidze@users.noreply.github.com>
2022-07-18 17:35:15 +0300

    infinite scroll added

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { MenuController } from '@ionic/angular';
+import { IonInfiniteScroll, MenuController } from '@ionic/angular';
 import { HttpService } from '../services/http.service';
 import { Field } from '../shared/classes';
 import { ProductModel } from '../shared/models';
@@ -13,23 +13,45 @@ import { ProductModel } from '../shared/models';
 export class Tab1Page implements OnInit {
 
   constructor(private menu: MenuController,
-    private http: HttpService, private router: ActivatedRoute) { }
+    private http: HttpService, private router: ActivatedRoute) {
 
-  products: ProductModel[];
+
+  }
+  @ViewChild(IonInfiniteScroll, { static: false }) infiniteScroll: IonInfiniteScroll;
+  products:ProductModel[];
+
   field: Field = new Field();
- 
+  itemLength: any = 6;
+  
+  doInfinite(infiniteScroll) {
+    this.itemLength += 6
+    this.returnProducts();
+    setTimeout(() => {
+      this.infiniteScroll.complete();
+    }, 500);
+  }
+
+
+
+
 
   ngOnInit(): void {
-    this.field.search='';
-    
-
+    this.field.search = '';
     this.returnProducts();
 
   }
   returnProducts() {
     this.http.getProducts().subscribe((res) => {
-      this.products = res;
+      let items = res;
+      this.products = items.slice(0, this.itemLength);
     })
+
+
+
+
+
+
+
 
   }
 
@@ -37,28 +59,25 @@ export class Tab1Page implements OnInit {
   openFirst() {
     this.menu.enable(true, 'first');
     this.menu.open('first');
-  }
+  };
 
   openEnd() {
     this.menu.open('end');
-  }
+  };
 
   openCustom() {
     this.menu.enable(true, 'custom');
     this.menu.open('custom');
-  }
+  };
 
 
   search(form) {
-    if(form.invalid){
+    if (form.invalid) {
       return
     }
-    else{
+    else {
       console.log(form)
+    };
+  };
 
-    }
-  }
-
-
-
-}
+};
